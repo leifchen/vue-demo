ae406524cf7c688684c2b4feed3d678176394cbe
LeifChen <leifchen90@gmail.com>
2018-11-02 10:19:29 +0800

    feat: add header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <v-header></v-header>
+    <v-header :seller="seller"></v-header>
     <div class="tab border-1px">
       <div class="tab-item">
         <router-link to="/goods">商品</router-link>
@@ -20,15 +20,31 @@
 <script>
 import header from './components/header/header.vue';
 
+const ERR_OK = 0;
+
 export default {
+  data() {
+    return {
+      seller: {}
+    };
+  },
+  created() {
+    this.$http.get('/api/seller').then(response => {
+      response = response.body;
+      if (response.errno === ERR_OK) {
+        this.seller = response.data;
+        console.log(this.seller);
+      }
+    });
+  },
   components: {
     'v-header': header
   }
 };
 </script>
 
-<style lang="stylus" rel="stylesheet/stylus">
-@import "./common/stylus/mixin.styl";
+<style lang="stylus">
+@import './common/stylus/mixin';
 
 #app {
   .tab {
@@ -36,7 +52,6 @@ export default {
     witdh: 100%;
     height: 40px;
     line-height: 40px;
-    // border-bottom: 1px solid rgba(7, 17, 27, 0.1);
     border-1px(rgba(7, 17, 27, 0.1));
 
     .tab-item {
