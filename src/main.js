dafcbb69a993e070a7dd806a1db9fa9b1be3c03b
LeifChen <leifchen90@gmail.com>
2018-11-06 11:31:38 +0800

    refactor: update router

--- src/main.js
+++ src/main.js
@@ -1,42 +1,15 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
 import App from './App';
-import goods from 'components/goods/goods';
-import ratings from 'components/ratings/ratings';
-import seller from 'components/seller/seller';
+import router from './router/index.js';
+
 import 'common/stylus/index.styl';
 
-Vue.config.productionTip = false;
-
-Vue.use(VueRouter);
 Vue.use(VueResource);
 
-// 定义路由
-const routes = [{
-  path: '/goods',
-  component: goods
-}, {
-  path: '/ratings',
-  component: ratings
-}, {
-  path: '/seller',
-  component: seller
-}];
-
-// 创建 router 实例
-const router = new VueRouter({
-  linkActiveClass: 'active',
-  routes
-});
-
-// 创建和挂载根实例
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: {
-    App
-  },
-  template: '<App/>',
-  router
-}).$mount('#app');
+  router,
+  render: h => h(App)
+});
---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import goods from 'components/goods/goods';
+import ratings from 'components/ratings/ratings';
+import seller from 'components/seller/seller';
+
+Vue.use(Router);
+
+// 定义路由
+const routes = [{
+  path: '/',
+  redirect: '/goods'
+}, {
+  path: '/goods',
+  component: goods
+}, {
+  path: '/ratings',
+  component: ratings
+}, {
+  path: '/seller',
+  component: seller
+}];
+
+// 创建 router 实例
+export default new Router({
+  linkActiveClass: 'active',
+  routes
+});
